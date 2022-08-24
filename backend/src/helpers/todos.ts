dd8c43f5b0ae43d385321cca732e967210869eac
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-08-24 11:57:46 +0100

    first commit

---
+++ backend/src/helpers/todos.ts
@@ -0,0 +1,73 @@
+import { TodosAccess } from './todosAcess'
+import { AttachmentUtils } from './attachmentUtils';
+import { TodoItem } from '../models/TodoItem'
+import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+import { createLogger } from '../utils/logger'
+import * as uuid from 'uuid'
+import { TodoUpdate } from '../models/TodoUpdate';
+
+// TODO: Implement businessLogic
+const logger = createLogger('todos')
+const bucketName = process.env.ATTACHMENT_S3_BUCKET
+
+const todosAcess = new TodosAccess()
+
+export async function getTodosForUser(userId: string):Promise<TodoItem[]> {
+	logger.info('Getting all Todos')
+	return todosAcess.getAllTodos(userId)
+}
+
+export async function createTodo(
+	createTodoRequest: CreateTodoRequest,
+	userId: string): Promise<TodoItem> {
+
+	const itemId = uuid.v4()
+	logger.info('Create Todo')
+
+	return await todosAcess.createTodo({
+		userId: userId,
+		todoId: itemId,
+		createdAt: new Date().toISOString(),
+		name: createTodoRequest.name,
+		dueDate: createTodoRequest.dueDate,
+		done: false,
+	})
+}
+
+export async function updateTodo(
+	updateTodoRequest: UpdateTodoRequest, todoId: string, userId: string) {
+		logger.info('Update Todo')
+		const update: TodoUpdate = {
+			name: updateTodoRequest.name,
+			dueDate: updateTodoRequest.dueDate,
+			done: updateTodoRequest.done
+		}
+
+		return await todosAcess.updateTodo(
+		todoId, userId, update)
+}
+
+export async function deleteTodo(
+	todoId: string, userId: string) {
+		logger.info('Delete Todo')
+		return await todosAcess.deleteTodoItem(
+		todoId,
+		userId
+	)
+}
+
+export async function createAttachmentPresignedUrl(
+	todoId: string, userId: string) {
+	logger.info('Generate presignedurl')
+
+	const imageId = userId + todoId
+
+	await todosAcess.PresignedUrl(
+		todoId,
+		userId,
+		`https://${bucketName}.s3.amazonaws.com/${imageId}`
+	)
+	logger.info('storing image')
+	return await AttachmentUtils(imageId)
+}
---
+++ backend/src/lambda/http/updateTodo.ts
@@ -0,0 +1,39 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+import { createLogger } from '../../utils/logger'
+import { updateTodo } from '../../helpers/todos'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { getUserId } from '../utils'
+
+const logger = createLogger('updateTodo')
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+    // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+		logger.info(`update event: ${event}`)
+		const userId = getUserId(event)
+
+		await updateTodo(updatedTodo, todoId, userId)
+
+    return {
+			statusCode: 201,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+			},
+			body: ''
+		}
+	}
+)
+
+handler
+ .use(httpErrorHandler())
+ .use(
+     cors({
+       credentials: true
+  })
+)
